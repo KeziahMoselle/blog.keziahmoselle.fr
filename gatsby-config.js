fdf08dd406bad3300155943fde391481ac52e01e
KeziahMoselle <keziah.moselle@gmail.com>
2019-02-20 01:39:09 +0100

    config: update pwa, favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,13 +51,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Blog - Keziah MOSELLE`,
+        short_name: `keziah blog`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-offline`,
