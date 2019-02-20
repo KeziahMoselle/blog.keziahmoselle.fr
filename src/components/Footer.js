449e85f65b47456c12f586e1bd2fbed64f8d7d2d
KeziahMoselle <keziah.moselle@gmail.com>
2019-02-20 02:36:39 +0100

    feat: footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,23 @@ function Footer () {
   return (
     <footer>
       <div className="container">
-        <h2>Footer</h2>
+        <p>
+          Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 5 ans.
+        </p>
+        
+        <ul>
+          <li>
+            <a href="https://github.com/KeziahMoselle">GitHub</a>
+          </li>
+
+          <li>
+            <a href="https://twitter.com/KeziahMoselle">Twitter</a>
+          </li>
+
+          <li>
+            <a href="https://keziahmoselle.fr/">Portfolio</a>
+          </li>
+        </ul>
       </div>
     </footer>
   )
