c66a3033cc3bc7d02ef7a0b6f4f99e45bea42ee2
KeziahMoselle <keziah.moselle@gmail.com>
2019-02-20 04:35:04 +0100

    fix: capitalize

---
+++ src/components/Layout.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Header from './Header'
+import Footer from './Footer'
+
+function Layout ({ location, children }) {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  if (location.pathname === rootPath) {
+    header = <Header isHomepage />
+  } else {
+    header = <Header />
+  }
+
+  return (
+    <div id="root">
+      {header}
+
+      <main className="container medium">
+        {children}
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
