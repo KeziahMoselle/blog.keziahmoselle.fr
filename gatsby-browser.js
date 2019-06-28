b74040653bcd5320c570a6e43aff37088cb032f8
Keziah <keziah.moselle@gmail.com>
2019-06-28 12:13:40 +0200

    fix: service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,3 @@
 import './src/utils/index.css'
 import './src/utils/markdown.css'
 import 'horizon-prismjs-theme/theme.css'
-
-export const onServiceWorkerUpdateReady = () => {
-  window.location.reload()
-}
