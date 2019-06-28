425879f44090f68beb6a69ff66e3cfcddac5f3d2
Keziah <keziah.moselle@gmail.com>
2019-06-28 11:37:51 +0200

    feat: service worker update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
 import './src/utils/index.css'
 import './src/utils/markdown.css'
 import 'horizon-prismjs-theme/theme.css'
+
+export const onServiceWorkerUpdateReady = () => {
+  window.location.reload()
+}
