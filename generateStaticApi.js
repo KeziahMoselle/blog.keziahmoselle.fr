a6135d4e0ab85bb2470e7fd0f259ecb449c517be
Nemesis <keziah.moselle@gmail.com>
2020-05-22 19:24:25 +0200

    feat: log

--- generateStaticApi.js
+++ generateStaticApi.js
@@ -1,4 +1,4 @@
-const fs = require('fs/promises')
+const fs = require('fs').promises
 const matter = require('gray-matter');
 const path = require('path')
 
@@ -6,30 +6,35 @@ const BLOG_PATH = path.join('content', 'blog')
 const STATIC_API_PATH = path.join('public', 'api', 'lastPost.json')
 
 async function readFolders () {
-  const folders = await fs.readdir(BLOG_PATH)
-  const allDirs = folders.map(folder => fs.readFile(path.join(BLOG_PATH, folder, 'index.md'), 'utf-8'))
-  const allPosts = await Promise.all(allDirs)
+  try {
+    const folders = await fs.readdir(BLOG_PATH)
+    const allDirs = folders.map(folder => fs.readFile(path.join(BLOG_PATH, folder, 'index.md'), 'utf-8'))
+    const allPosts = await Promise.all(allDirs)
 
-  let lastPost = null
+    let lastPost = null
 
-  for (const post of allPosts) {
-    const content = await matter(post)
-    
-    // If first blog post, set to lastPost by default
-    if (!lastPost) {
-      lastPost = content
-      continue
+    for (const post of allPosts) {
+      const content = await matter(post)
+      
+      // If first blog post, set to lastPost by default
+      if (!lastPost) {
+        lastPost = content
+        continue
+      }
+
+      const lastPostDate = new Date(lastPost.data.date)
+      const currentPostDate = new Date(content.data.date)
+
+      if (currentPostDate > lastPostDate) {
+        lastPost = content
+      }
     }
 
-    const lastPostDate = new Date(lastPost.data.date)
-    const currentPostDate = new Date(content.data.date)
-
-    if (currentPostDate > lastPostDate) {
-      lastPost = content
-    }
+    await fs.writeFile(STATIC_API_PATH, JSON.stringify(lastPost, null, 2))
+    console.log(`Successfully written to ${STATIC_API_PATH}`)
+  } catch (error) {
+    console.error(error)
   }
-
-  await fs.writeFile(STATIC_API_PATH, JSON.stringify(lastPost, null, 2))
 }
 
 
