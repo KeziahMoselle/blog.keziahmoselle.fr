64230f3d4bf8fd9b84dc8b0b7b5c2af3bc218f6a
Nemesis <keziah.moselle@gmail.com>
2020-05-26 17:59:12 +0200

    feat: excerpt and slug

--- generateStaticApi.js
+++ generateStaticApi.js
@@ -5,16 +5,20 @@ const path = require('path')
 const BLOG_PATH = path.join('content', 'blog')
 const STATIC_API_PATH = path.join('public', 'api')
 
+readFolders()
+
 async function readFolders () {
   try {
     const folders = await fs.readdir(BLOG_PATH)
-    const allDirs = folders.map(folder => fs.readFile(path.join(BLOG_PATH, folder, 'index.md'), 'utf-8'))
+    const allDirs = folders.map(folder => readFile(folder))
     const allPosts = await Promise.all(allDirs)
 
     let lastPost = null
 
     for (const post of allPosts) {
-      const content = await matter(post)
+      const content = await matter(post.content)
+      content.excerpt = getExcerpt(content.content)
+      content.slug = post.folder
       
       // If first blog post, set to lastPost by default
       if (!lastPost) {
@@ -31,6 +35,7 @@ async function readFolders () {
     }
 
     await fs.mkdir(STATIC_API_PATH)
+
     await fs.writeFile(
       path.join(STATIC_API_PATH, 'lastPost.json'),
       JSON.stringify(lastPost, null, 2)
@@ -41,5 +46,17 @@ async function readFolders () {
   }
 }
 
+function getExcerpt (str, limit = 240) {
+  if (!str) return
+  const lastWordIndex = str.lastIndexOf(' ', limit)
+  return `${str.substr(0, lastWordIndex)}...`
+}
 
-readFolders()
+async function readFile (folder) {
+  const content = await fs.readFile(path.join(BLOG_PATH, folder, 'index.md'), 'utf-8')
+
+  return {
+    content,
+    folder
+  }
+}
